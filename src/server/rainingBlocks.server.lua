cb0c2da39dfa273b9e9b0a16118fc5a21ffe8ebc
RainyLofi <U1959985@unimail.hud.ac.uk>
2021-05-23 22:29:03 +0100

    No more negatively sized parts
    Minimum size is now 1, 1, 1

--- src/server/rainingBlocks.server.lua
+++ src/server/rainingBlocks.server.lua
@@ -15,7 +15,7 @@ local Settings = {
 while wait(1) do
     for i = 1, 10 do
         local Part = Instance.new('Part', Workspace)
-        Part.Size = Vector3.new(math.random(-Settings.MaxX / 2, Settings.MaxX / 2), math.random(-Settings.MaxY / 2, Settings.MaxY / 2), math.random(-Settings.MaxZ / 2, Settings.MaxZ / 2))
+        Part.Size = Vector3.new(math.random(1, Settings.MaxX / 2), math.random(1, Settings.MaxY / 2), math.random(1, Settings.MaxZ / 2))
         Part.CFrame = CFrame.new(math.random(-Settings.MaxPosX / 2, Settings.MaxPosX / 2), Settings.PosY, math.random(-Settings.MaxPosZ / 2, Settings.MaxPosZ / 2))
         Part.BrickColor = BrickColor.Random()
         Debris:AddItem(Part, 20)
